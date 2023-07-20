818336cea94e6da63b7e99c5b90895828ecb26b6
Del Piero Flores <delpier22.flores@gmail.com>
2023-07-20 15:59:48 -0500

    u

--- gps_only.py
+++ gps_only.py
@@ -209,7 +209,7 @@ def main():
     # This is just an example, do whatever you do
     t_end = time.time() + 60 * 15
     while time.time() < t_end:
-        # logger.info(f"Position: {gnss.get_pos()}")
+        logger.info(f"Position: {gnss.get_pos()}")
         time.sleep(SLEEP_TIME)
 
 if __name__ == "__main__":
