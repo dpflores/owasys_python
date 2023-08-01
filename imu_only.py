7c9d6b212048f035a15b40ff69b4f7269e71a4c1
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-01 16:04:48 -0500

    u

--- imu_only.py
+++ imu_only.py
@@ -139,7 +139,8 @@ class RTU:
 
         
         while True:
-            ret = self.libRtu.RTU_CfgMovementSensor(c_ubyte(0),c_ubyte(100),c_ubyte(5),self.pmovehandler)
+            ret = self.libRtu.RTU_CfgMovementSensor(c_ubyte(0),c_ubyte(100),c_ubyte(50),self.pmovehandler)
+            ret = self.libRtu.RTU_CfgMovementSensor(0,c_ubyte(100),c_ubyte(50),self.pmovehandler)
             logging.info(ret)
             if ret != 0:
                 break
