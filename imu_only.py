7418504a8cb1b020be220907ce1e1816a107500b
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-01 15:15:12 -0500

    u

--- imu_only.py
+++ imu_only.py
@@ -136,7 +136,7 @@ class RTU:
         self.movehandler = MOVE_INT_T()
         self.pmovehandler = pointer(self.movehandler)
 
-        ret = self.libRtu.RTU_CfgMovementSensor(c_char(0),c_char(127),c_char(5),self.pmovehandler)
+        ret = self.libRtu.RTU_CfgMovementSensor(c_char(1),c_char(127),c_char(5),self.pmovehandler)
         
         self.set_accel()
 
