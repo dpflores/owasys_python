6f779ce4d7d59891da46a25d88636c76f142985f
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-02 11:09:46 -0500

    u

--- imu_only.py
+++ imu_only.py
@@ -151,12 +151,12 @@ class RTU:
         logging.info(ret)
         
         
-        self.set_accel()
+        # self.set_accel()
         logging.info("Modulo iniciado correctamente")
 
     def set_accel(self):
-        self.libRtu.RTU_GetRawAcceleration.restype = c_int
-        self.libRtu.RTU_GetRawAcceleration.argtypes=[POINTER(MOVE_INT_T)]
+        # self.libRtu.RTU_GetRawAcceleration.restype = c_int
+        # self.libRtu.RTU_GetRawAcceleration.argtypes=[POINTER(MOVE_INT_T)]
         self.acceldata = MOVE_INT_T()
         self.pacceldata= pointer(self.acceldata)
         
