0417216e550658e026bd3c03575626d775ebae1c
Del Piero Flores <delpier22.flores@gmail.com>
2023-08-02 14:30:59 -0500

    u

--- imu_only_nodered.py
+++ imu_only_nodered.py
@@ -272,6 +272,7 @@ def main():
     while True:
         # data = rtu.get_raw_accel()
         data = rtu.get_move_sensor()
+        print(data)
         del data["scale"]
         print(data)
         # print(json.dumps(data_string))
